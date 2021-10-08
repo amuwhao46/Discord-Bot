71c0d0faf2037ab34dcad41b750ab5c38c9b1904
Oke Amuwha <amuwhao46@gmail.com>
2021-10-08 12:36:44 -0400

    Added another command

--- main.js
+++ main.js
@@ -18,8 +18,11 @@ client.on('message', message =>{
     const command = args.shift().toLowerCase(); // takes prefix out
 
     // Display's to user
-    if(command === 'ping')
+    if(command == 'ping')
         message.channel.send('pong!')
+    else if (command == 'YouTube') {
+        message.channel.send('https://YouTube.com/watch?v=dQw4w9WgXcQ')
+    }
 });
 
 
