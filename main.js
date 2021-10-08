d380a902c282274c527059e046eb16ceeb3eb128
Oke Amuwha <amuwhao46@gmail.com>
2021-10-08 12:34:24 -0400

    Added variables to make the bot work
    Added a prefix and command slicer to seperate the command from the message to make an action. When user types %command the bot will respond. for example %ping will prompt the bot to respond pong

--- main.js
+++ main.js
@@ -1,9 +1,28 @@
 const { Client, Intents } = require('discord.js');    
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] }); 
+const prefix = '%' // Bot Prefix
 
+
+// Bot is online
 client.once('ready', ()=> {
     console.log('Bill Bob is here!')
 })
 
-// test
+// check
+client.on('message', message =>{
+    // Checks if message starts with prefix or if bot sent prefix
+    if(!message.content.startsWith(prefix) || message.author.bot)  return;
+
+    // command slicer
+    const args = message.content.slice(prefix.length).split(/ +/);
+    const command = args.shift().toLowerCase(); // takes prefix out
+
+    // Display's to user
+    if(command === 'ping')
+        message.channel.send('pong!')
+});
+
+
+
+
 client.login('[REDACTED]')
